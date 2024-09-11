788d632e37d1d512792f3201a2d0c7076d918472
say-lem <nedusalemonah@gmail.com>
2024-09-11 11:13:47 +0100

    Tomorrow added

--- src/Pages/Eservicess/Esevices.jsx
+++ src/Pages/Eservicess/Esevices.jsx
@@ -4,7 +4,7 @@ import { Tomorrow } from "../../components/Tomorrow";
 
 export const Eservices = () => {
   return (
-    <div className=" text-black overflow-hidden max-w-screen min-h-screen">
+    <div className=" text-black overflow-hidden max-w-screen ">
       <div className="md:pt-24 md:p-5 pt-20 p-3 container">
         <Hero
           title="e-Services"
--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -5,7 +5,7 @@ import { Tomorrow } from "../../components/Tomorrow";
 export const Projects = () => {
   return (
     <div className=" text-black ">
-      <div className="container md:pt-24 md:p-5 pt-20 p-3 overflow-hidden max-w-screen min-h-screen">
+      <div className="container md:pt-24 md:p-5 pt-20 p-3 overflow-hidden max-w-screen ">
         <Hero
           title="Our Projects"
           text="Explore a selection of our latest and greatest projects. Each one showcases our commitment to innovation and design excellence."
