900124e4af773e7f731078882619831a62e1db5b
say-lem <nedusalemonah@gmail.com>
2024-09-11 11:12:21 +0100

    Tomorrow added

--- src/Pages/OurServices/OurServices.jsx
+++ src/Pages/OurServices/OurServices.jsx
@@ -1,14 +1,16 @@
 import { Hero } from "../../components/About/Hero"
 import { Services } from "../../components/OurSevice/services"
+import { Tomorrow } from "../../components/Tomorrow"
 
 export const OurServices = () =>{
     return(
-        <div className="md:pt-24 md:p-5 pt-20 p-3 text-black overflow-hidden max-w-screen min-h-screen">
-            <div className="container">
+        <div className="text-black ">
+            <div className="container md:pt-24 md:p-5  pt-20 p-3  overflow-hidden max-w-screen">
 
             <Hero title="Our Sevices"/>
             <Services />
             </div>
+            <Tomorrow />
         </div>
     )
 }
---
+++ src/components/Tomorrow.jsx
@@ -0,0 +1,23 @@
+import Button from "./Button";
+import { FaPhoneAlt } from "react-icons/fa";
+
+export const Tomorrow = () => {
+  return (
+    <div className="flex items-center md:p-24 py-24 px-8 justify-center bg-green-100">
+
+    <div className="flex flex-col items-center gap-4 md:max-w-[500px]">
+      <h1 className="text-green-900 font-extrabold text-[28px]">Our tomorrow is Here</h1>
+      <p className="text-center">
+        Take the first step towards a sustainable future. Contact us today for a
+        free consultation.
+      </p>
+      <div>
+        <button className="bg-green-400 flex item-center text-white p-3 px-11 md:px-16 gap-3 rounded-lg">
+          <FaPhoneAlt />
+          Contact Us
+        </button>
+      </div>
+    </div>
+    </div>
+  );
+};
