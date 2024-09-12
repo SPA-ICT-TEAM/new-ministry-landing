7131fa4825dec507426ed19f767c0956928d0f7a
saammz <ghostcybertron@gmail.com>
2024-09-12 11:20:12 +0100

    updated project

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -5,32 +5,42 @@ import { serviceData } from "../../constants";
 import Button from "../Button";
 
 const ProjectDrawer = ({ isOpen, onClose, selectedProject }) => {
+
   return (
     <div
       className={`fixed inset-0 bg-gray-600 bg-opacity-50 z-50 transition-transform transform ${
         isOpen ? "translate-x-0" : "translate-x-full"
       }`}
     >
-      <div className="w-80 bg-white h-full p-5 shadow-lg absolute right-0 top-0">
-        <h2 className="absolute top-2 w-full right-0 text-center shadow-md p-3 md:text-[24px] font-bold">
+      <div className="w-80 md:w-[28rem] lg:w-[32rem] xl:w-[36rem] bg-white h-full p-5 shadow-lg absolute right-0 top-0 flex flex-col justify-between">
+        <div>
+          <h2 className="absolute top-2 w-full right-0 text-center shadow-md p-3 md:text-[24px] font-bold">
           Project Details
-        </h2>
-        {selectedProject ? (
-          <div className="pt-11">
-            <img
-              src={selectedProject.image || logo}
-              alt={selectedProject.title}
-              className="w-full object-cover mb-4"
-            />
-            <h2 className="text-xl font-bold">{selectedProject.title}</h2>
-            <h3>{selectedProject.text}</h3>
-            <Button onClick={onClose} className="mt-4 bg-red-400">
-              Close
-            </Button>
-          </div>
-        ) : (
-          <p>No project selected.</p>
-        )}
+          </h2>
+
+          {selectedProject ? (
+            <div className="pt-11">
+              <div>
+                <img
+                  src={selectedProject.image || logo}
+                  alt={selectedProject.titlee}
+                  className="w-full object-cover rounded-3xl mb-4"
+                />
+                <h2 className="text-xl font-bold">{selectedProject.title}</h2>
+                <h3>{selectedProject.text}</h3>
+              </div>
+            </div>
+          ) : (
+            <p>No project selected.</p>
+          )}
+        </div>
+
+        {/* Buttons placed at the bottom */}
+        <div className="flex justify-between mt-auto pt-4">
+          <Button onClick={onClose} className="bg-red-400">
+            Close
+          </Button>
+        </div>
       </div>
     </div>
   );
