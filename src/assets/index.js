4f5e60e6bb338e7e59c8b991aed3764804824a88
saammz <ghostcybertron@gmail.com>
2024-09-12 11:44:10 +0100

    removed un-used assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,14 +1,3 @@
-import brainwave from "./brainwave.svg";
-import bg from "./sample.jpg";
-import check from "./check.svg";
-import brainwaveSymbol from "./brainwave-symbol.svg";
-import brainwaveWhiteSymbol from "./brainwave-symbol-white.svg";
-import play from "./play.svg";
-import gradient from "./gradient.png";
-import smallSphere from "./4-small.png";
-import grid from "./grid.png";
-import check2 from "./check-02.svg";
-import loading1 from "./loading-01.svg";
 import yourlogo from "./yourlogo.svg";
 import homeSmile from "./home-smile.svg";
 import file02 from "./file-02.svg";
@@ -19,11 +8,12 @@ import recording01 from "./recording-01.svg";
 import disc02 from "./disc-02.svg";
 import chromecast from "./chrome-cast.svg";
 import sliders04 from "./sliders-04.svg";
-import loading from "./loading.png";
 import mainLogo from "./logo.png";
-import background from "./background.jpg";
 
 import curve from "./hero/curve.png";
+import landingDefault from "./landing_default.jpg";
+import newsDefault from "./news_default.jpg";
+import projectDefault from "./project_default.jpg";
 
 import curve1 from "./collaboration/curve-1.svg";
 import curve2 from "./collaboration/curve-2.svg";
@@ -41,17 +31,7 @@ import benefitIcon4 from "./benefits/icon-4.svg";
 import benefitImage2 from "./benefits/image-2.png";
 
 export {
-  brainwave,
-  check,
-  check2,
-  loading1,
-  brainwaveSymbol,
-  brainwaveWhiteSymbol,
-  play,
-  gradient,
-  smallSphere,
-  grid,
-  yourlogo,
+  yourlogo,                     
   homeSmile,
   file02,
   searchMd,
@@ -61,8 +41,6 @@ export {
   disc02,
   chromecast,
   sliders04,
-  loading,
-  background,
   curve,
   curve1,
   curve2,
@@ -78,5 +56,7 @@ export {
   benefitIcon4,
   benefitImage2,
   mainLogo,
-  bg,
+  landingDefault,
+  newsDefault,
+  projectDefault,
 };
